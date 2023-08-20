df46a974f6b9e014f544ef439e9ad5a874056a9f
unknown <mabrukamustafam@gmail.com>
2023-08-20 19:04:36 +0300

    answered

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-// Good Luck 💪🏾
+function removeduplicates(number) {
+
+    const duplicate=[];
+
+    for (let i = 0; i < number.length; i++) {
+        if (!duplicate.includes(number[i])) {
+
+            duplicate.push(number[i]);
+            
+        }
+        
+    }
+    return duplicate;
+}
+
+console.log(removeduplicates([1,0,1,0]));
+
+console.log(removeduplicates(["New", "York", "City"]));
+
+console.log(removeduplicates(["Hassan", "Aisha", "Aisha"]));
